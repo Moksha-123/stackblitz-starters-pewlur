497b00ec807d2744ea4b2c1a74ec3667b656a838
Moksha-123 <141109376+Moksha-123@users.noreply.github.com>
2023-08-08 12:01:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mokshawhatsapp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-nrkpmh)
---
+++ src/main/main.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ConversationComponent } from './conversation/conversation.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
+@Component({
+  standalone:true,
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.css'],
+  imports:[SidenavComponent,ConversationComponent],
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
